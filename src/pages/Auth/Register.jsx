deed59f74b9f6c4dfeee20be654fe73116711e1a
Abby Hilman <abbyhilman@gmail.com>
2021-08-09 10:47:56 +0700

    update for validate email

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -10,6 +10,7 @@ const Register = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [alerttest, setAlertTest] = useState(false);
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
@@ -21,30 +22,54 @@ const Register = () => {
     }
   };
 
+  const validateEmail = (email) => {
+    const pattern = /[a-zA-Z0-9]+[\.]?([a-zA-Z0-9]+)?[\@][a-z]{3,9}[\.][a-z]{2,5}/g;
+    const result = pattern.test(email);
+    if (result === true) {
+      setAlertTest(false);
+      dispatch(registerUser({ fullname, username, email, password }));
+    } else {
+      setAlertTest(true);
+    }
+  };
+
   const submitCheck = () => {
     if (!username.length) {
-      alert("Please enter your username");
+      setAlertTest(true);
     } else if (!fullname.length) {
-      alert("Please enter your full name");
+      setAlertTest(true);
     } else if (!email.length) {
-      alert("Please enter your email");
+      setAlertTest(true);
     } else if (!password.length) {
-      alert("Please enter your password");
-    } else {
-      dispatch(registerUser({ fullname, username, email, password }));
+      setAlertTest(true);
+    } else if (email.length) {
+      validateEmail(email);
     }
   };
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
-        {user.errMsg ? (
-          <div role="alert">
-            <div className="bg-red-500 text-white font-bold rounded-t px-4 py-2">
-              Error
-            </div>
-            <div className="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
-              <p>{user.errMsg}</p>
-            </div>
+        {alerttest ? (
+          <div
+            class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+            role="alert"
+          >
+            <strong class="font-bold">Error </strong>
+            <span class="block sm:inline">Please Complete the form</span>
+            <span
+              class="absolute top-0 bottom-0 right-0 px-4 py-3"
+              onClick={() => setAlertTest(false)}
+            >
+              <svg
+                class="fill-current h-6 w-6 text-red-500"
+                role="button"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <title>Close</title>
+                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+              </svg>
+            </span>
           </div>
         ) : null}
         <div>
