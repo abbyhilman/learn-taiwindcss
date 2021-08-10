da73da5d87220192a3028a2e30d42e4200e1c059
Abby Hilman <abbyhilman@gmail.com>
2021-08-10 12:58:29 +0700

    use loading for user login and register

--- src/redux/actions/cart.js
+++ src/redux/actions/cart.js
@@ -1,14 +1,17 @@
 import Axios from "axios";
 import { API_URL } from "../../constants/API";
+import { setLoading } from "./global";
 
 export const getCartData = (userId) => {
   return (dispatch) => {
+    dispatch(setLoading(true));
     Axios.get(`${API_URL}/carts`, {
       params: {
         userId,
       },
     })
       .then((res) => {
+        dispatch(setLoading(false));
         // Dispatch to cart reducer with payload -> res.data
         dispatch({
           type: "FILL_CART",
@@ -16,6 +19,7 @@ export const getCartData = (userId) => {
         });
       })
       .catch((err) => {
+        dispatch(setLoading(false));
         alert("terjadi kesalahan diserver!");
       });
   };
--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -1,8 +1,10 @@
 import Axios from "axios";
 import { API_URL } from "../../constants/API";
+import { setLoading } from "./global";
 
 export const registerUser = ({ fullname, username, email, password }) => {
   return (dispatch) => {
+    dispatch(setLoading(true));
     Axios.post(`${API_URL}/users`, {
       fullname,
       username,
@@ -12,13 +14,16 @@ export const registerUser = ({ fullname, username, email, password }) => {
     })
       .then((res) => {
         delete res.data.password;
+        dispatch(setLoading(false));
         dispatch({
           type: "USER_LOGIN",
           payload: res.data,
         });
+
         alert("Berhasil mendaftarkan user!");
       })
       .catch((err) => {
+        dispatch(setLoading(false));
         alert("Gagal mendaftarkan user!");
       });
   };
@@ -26,12 +31,14 @@ export const registerUser = ({ fullname, username, email, password }) => {
 
 export const loginUser = ({ username, password }) => {
   return (dispatch) => {
+    dispatch(setLoading(true));
     Axios.get(`${API_URL}/users`, {
       params: { username },
     })
       .then((res) => {
         if (res.data.length) {
           if (password === res.data[0].password) {
+            dispatch(setLoading(false));
             delete res.data[0].password;
             localStorage.setItem(
               "userDataEmmerce",
@@ -42,6 +49,7 @@ export const loginUser = ({ username, password }) => {
               payload: res.data[0],
             });
           } else {
+            dispatch(setLoading(false));
             // Handle error worng password
             dispatch({
               type: "USER_ERROR",
@@ -49,6 +57,7 @@ export const loginUser = ({ username, password }) => {
             });
           }
         } else {
+          dispatch(setLoading(false));
           // Handle error username not found
           dispatch({
             type: "USER_ERROR",
@@ -57,6 +66,7 @@ export const loginUser = ({ username, password }) => {
         }
       })
       .catch((err) => {
+        dispatch(setLoading(false));
         alert("terjadi kesalahan diserver!");
       });
   };
